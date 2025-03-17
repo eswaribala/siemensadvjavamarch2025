abcd1847d4fc1652102978b487aaac85259edf8d
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-03-17 15:56:25 +0530

    updated

--- advjava21/src/main/java/com/siemens/views/StreamDemo.java
+++ advjava21/src/main/java/com/siemens/views/StreamDemo.java
@@ -3,7 +3,9 @@ package com.siemens.views;
 import com.siemens.dtos.IndividualDTO;
 import com.siemens.models.Individual;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class StreamDemo {
@@ -23,16 +25,26 @@ public class StreamDemo {
                 .count();
        System.out.println("Number of people born before 2000=>"+count);
 
-      // sorting
+      // sorting and creating the list
 
       List<IndividualDTO> individualDTOList=  IndividualComparatorDemo.generateIndividuals().stream()
                 .filter(i->i.getDateOfBirth().getYear()<2000)
                 .sorted((obj1,obj2)->{return obj1.getDateOfBirth().compareTo(obj2.getDateOfBirth());})
-                .map(individual -> new IndividualDTO(individual.getFullName().getFirstName(),individual.getDateOfBirth()))
+                .map(individual -> new IndividualDTO(individual.getFullName().getFirstName(),
+                        individual.getDateOfBirth()))
                 .toList();
 
       individualDTOList.forEach(System.out::println);
 
+      //sort and create map
+      Map<String, LocalDate> map=  IndividualComparatorDemo.generateIndividuals().stream()
+                .filter(i->i.getDateOfBirth().getYear()<2000)
+                .sorted((obj1,obj2)->{return obj1.getDateOfBirth().compareTo(obj2.getDateOfBirth());})
+                .collect(Collectors.toMap(i->i.getFullName().getFirstName(),
+                        Individual::getDateOfBirth));
+
+      map.entrySet().stream().forEach(entrySet->
+              System.out.println(entrySet.getKey()+" "+entrySet.getValue()));
 
     }
 }
