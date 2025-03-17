53f79b75f3c53157592a80768b14ab837b8c45d9
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-03-17 15:29:08 +0530

    updated

--- advjava21/src/main/java/com/siemens/views/StreamDemo.java
+++ advjava21/src/main/java/com/siemens/views/StreamDemo.java
@@ -19,8 +19,13 @@ public class StreamDemo {
                 .count();
        System.out.println("Number of people born before 2000=>"+count);
 
-       
+      // sorting
 
+        IndividualComparatorDemo.generateIndividuals().stream()
+                .filter(i->i.getDateOfBirth().getYear()<2000)
+                .sorted((obj1,obj2)->{return obj1.getDateOfBirth().compareTo(obj2.getDateOfBirth());})
+
+                .forEach(System.out::println);
 
 
     }
