2ea41b2091b07b5ecd3799ce3384692a8e48b395
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-03-17 13:20:57 +0530

    updated

--- advjava21/src/main/java/com/siemens/views/BuiltInFunctionalInterfacesDemo.java
+++ advjava21/src/main/java/com/siemens/views/BuiltInFunctionalInterfacesDemo.java
@@ -1,6 +1,7 @@
 package com.siemens.views;
 
 import com.github.javafaker.Faker;
+import com.siemens.fadades.TriFunction;
 import com.siemens.models.FullName;
 import com.siemens.models.Individual;
 
@@ -27,10 +28,24 @@ public class BuiltInFunctionalInterfacesDemo {
                 .dateOfBirth(faker.date().birthday().toInstant().atZone(ZoneId.systemDefault()).toLocalDate())
                 .build();
         //Function
-        Function<Individual, LocalDate> function =(obj)-> obj.getDateOfBirth();
+       Function<Individual, LocalDate> function = Individual::getDateOfBirth;
        System.out.println("DOB="+function.apply(individual));
        BiFunction<Individual,String,Boolean> biFunction =(obj,str)->obj.getPassword().equals(str);
        System.out.println("Result="+biFunction.apply(individual,individual.getPassword()));
 
+        TriFunction<Individual,Individual,Individual,Boolean> triFunction =(obj1,obj2,obj3)->{
+            if(obj1.getPassword().equals(obj2.getPassword())){
+                if(obj2.getPassword().equals(obj3.getPassword())){
+                    return true;
+                }else
+                    return false;
+            }else
+                return false;
+
+        };
+
+        System.out.println(triFunction.apply(IndividualComparatorDemo.generateIndividuals().get(0),
+                IndividualComparatorDemo.generateIndividuals().get(1),
+                IndividualComparatorDemo.generateIndividuals().get(2)));
     }
 }
