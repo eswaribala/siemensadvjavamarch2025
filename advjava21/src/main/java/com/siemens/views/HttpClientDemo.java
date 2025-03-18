a7628d8b82a2f6708e17b3e1f9ac01d0b0df7c96
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-03-18 13:00:06 +0530

    updated

---
+++ advjava21/src/main/java/com/siemens/views/HttpClientDemo.java
@@ -0,0 +1,50 @@
+package com.siemens.views;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpHeaders;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+
+public class HttpClientDemo {
+    public static void main(String[] args) {
+
+        //step 1
+        HttpClient client = HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_2)
+                .connectTimeout(Duration.ofSeconds(5))
+                .build();
+
+        //step 2
+
+        HttpRequest request = HttpRequest.newBuilder().GET()
+                .uri(URI.create("https://restcountries.com/v2/all")) .build();
+
+        HttpResponse<String> response;
+        HttpHeaders headers;
+        JSONArray jsonArray;
+        try
+        {
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            headers = response.headers();
+            headers.map().entrySet().stream().forEach((entry)->{
+                System.out.println(entry.getKey()+ ": " + entry.getValue());
+            });
+           jsonArray = new JSONArray(response.body().toString());
+           for (int i = 0; i < jsonArray.length(); i++) {
+               JSONObject jsonObject = jsonArray.getJSONObject(i);
+               System.out.println(jsonObject.getString("name"));
+           }
+
+        }catch (Exception ignored){
+
+        }
+
+
+
+    }
+}
