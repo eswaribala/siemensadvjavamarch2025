7b2b49e884d0713cb15a11878e7749e52d47d7fe
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-03-19 15:45:01 +0530

    updated

---
+++ customerapi/src/main/java/com/siemens/customerapi/configurations/SwaggerConfiguration.java
@@ -0,0 +1,15 @@
+package com.siemens.customerapi.configurations;
+
+import com.siemens.customerapi.entities.Customer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+@Configuration
+public class SwaggerConfiguration implements RepositoryRestConfigurer {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+      //expose only available endpoints
+        config.setExposeRepositoryMethodsByDefault(false);
+    }
+}
