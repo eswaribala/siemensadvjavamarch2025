6026ad4bfeda7f9ddb5092c35afc2afd675c5bf8
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-03-19 16:55:44 +0530

    updated

--- customerapi/src/main/java/com/siemens/customerapi/dtos/GenericResponse.java
+++ customerapi/src/main/java/com/siemens/customerapi/dtos/GenericResponse.java
@@ -6,6 +6,10 @@ import lombok.Data;
 public class GenericResponse<T> {
     private T data;
     private String message;
-    public GenericResponse(T data) {}
-    public GenericResponse(String message) {}
+    public GenericResponse(T data) {
+        this.data = data;
+    }
+    public GenericResponse(String message) {
+        this.message = message;
+    }
 }
--- customerapi/src/main/java/com/siemens/customerapi/entities/FullName.java
+++ customerapi/src/main/java/com/siemens/customerapi/entities/FullName.java
@@ -1,5 +1,6 @@
 package com.siemens.customerapi.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,8 +13,11 @@ import lombok.NoArgsConstructor;
 @Builder
 @Embeddable
 public class FullName {
+    @Column(name="First_Name",nullable = false,length = 50)
     private String firstName;
+    @Column(name="Last_Name",nullable = false,length = 50)
     private String lastName;
+    @Column(name="Middle_Name",nullable = true,length = 50)
     private String middleName;
 
 }
