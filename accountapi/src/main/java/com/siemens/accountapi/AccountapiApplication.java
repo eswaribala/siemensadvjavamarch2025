c5c7985670568d2d928485e96cf50fa49362028e
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-03-20 17:08:14 +0530

    updated

--- accountapi/src/main/java/com/siemens/accountapi/AccountapiApplication.java
+++ accountapi/src/main/java/com/siemens/accountapi/AccountapiApplication.java
@@ -1,10 +1,14 @@
 package com.siemens.accountapi;
 
+import com.siemens.accountapi.configurations.VaultConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableConfigurationProperties(VaultConfiguration.class)
+
 @EnableDiscoveryClient
 public class AccountapiApplication {
 
