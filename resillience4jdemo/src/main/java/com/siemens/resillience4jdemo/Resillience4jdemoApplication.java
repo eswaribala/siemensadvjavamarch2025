a35a2ffa98029f5f7a2f5304948a0442af27f810
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-03-21 11:55:24 +0530

    updated

---
+++ resillience4jdemo/src/main/java/com/siemens/resillience4jdemo/Resillience4jdemoApplication.java
@@ -0,0 +1,13 @@
+package com.siemens.resillience4jdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Resillience4jdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Resillience4jdemoApplication.class, args);
+    }
+
+}
---
+++ resillience4jdemo/src/test/java/com/siemens/resillience4jdemo/Resillience4jdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siemens.resillience4jdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Resillience4jdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
